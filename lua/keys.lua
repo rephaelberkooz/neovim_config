fde2d264dc5418a5171187bc7af1cb584bc89a06
Rephael <rephael@Rephaels-MacBook-Pro.local>
2023-03-19 23:42:16 -0400

    initial commit

---
+++ lua/keys.lua
@@ -0,0 +1,10 @@
+--[[ keys.lua ]]
+local map = vim.api.nvim_set_keymap
+
+-- remap the key used to leave insert mode
+map('i', 'jk', '<Esc>', {})
+
+-- Toggle more plugins
+map('n', 'l', [[:IndentLinesToggle]], {})
+map('n', 't', [[:TagbarToggle]], {})
+map('n', 'ff', [[:Telescope find_files]], {})
